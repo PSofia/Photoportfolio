7263c1494405169d6380801f813e279124715126
Unknown <kotmot79@gmail.com>
2017-12-17 17:31:53 +0200

    Added DbInitializer and some test images.

---
+++ src/Photoportfolio/Infrastructure/DbInitializer.cs
@@ -0,0 +1,123 @@
+﻿using Photoportfolio.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Photoportfolio.Infrastructure
+{
+    public static class DbInitializer
+    {
+        private static PhotoPortfolioDbContext context;
+        public static void Initialize(IServiceProvider serviceProvider, string imagesPath)
+        {
+            context = (PhotoPortfolioDbContext)serviceProvider.GetService(typeof(PhotoPortfolioDbContext));
+
+            InitializePhotoAlbums(imagesPath);
+            InitializeUserRoles();
+        }
+
+        private static void InitializePhotoAlbums(string imagesPath)
+        {
+            if (!context.Albums.Any())
+            {
+                List<Album> albums = new List<Album>();
+
+                var album1 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 1",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+                var album2 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 2",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+                var album3 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 3",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+                var album4 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 4",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+
+                albums.Add(album1);
+                albums.Add(album2);
+                albums.Add(album3);
+                albums.Add(album4);
+
+                string[] images = Directory.GetFiles(Path.Combine(imagesPath, "images"));
+                Random rnd = new Random();
+
+                foreach (string image in images)
+                {
+                    int selectedAlbum = rnd.Next(1, 4);
+                    string fileName = Path.GetFileName(image);
+
+                    context.Photos.Add(
+                        new Photo()
+                        {
+                            Title = fileName,
+                            DateUploaded = DateTime.Now,
+                            Uri = fileName,
+                            Album = albums.ElementAt(selectedAlbum)
+                        }
+                        );
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void InitializeUserRoles()
+        {
+            if (!context.Roles.Any())
+            {
+                // create roles
+                context.Roles.AddRange(new Role[]
+                {
+                    new Role()
+                    {
+                        Name="Admin"
+                    }
+                });
+
+                context.SaveChanges();
+            }
+
+            if (!context.Users.Any())
+            {
+                context.Users.Add(new User()
+                {
+                    Email = "kotmot79@gmail.com",
+                    Username = "sophie",
+                    HashedPassword = "9wsmLgYM5Gu4zA/BSpxK2GIBEWzqMPKs8wl2WDBzH/4=",
+                    Salt = "GTtKxJA6xJuj3ifJtTXn9Q==",
+                    IsLocked = false,
+                    DateCreated = DateTime.Now
+                });
+
+                // create user-admin for chsakell
+                context.UserRoles.AddRange(new UserRole[] {
+                    new UserRole() {
+                        RoleId = 1, // admin
+                        UserId = 1  // sophie
+                    }
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
